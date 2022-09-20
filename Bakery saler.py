83d6d198abb93fe0d6b13588afb65e75aded9c26
dabbukotturaghu <112841745+dabbukotturaghu@users.noreply.github.com>
2022-09-20 11:18:18 +0530

    Add files via upload

---
+++ Bakery saler.py
@@ -0,0 +1,7 @@
+a=int(input("enter the no.of fresh breads:"))
+b=int(input("enter the no.of day old breads:"))
+c=int(input("regularprice:"))
+discount=c-((60/100)*c)
+print("the amount of fresh breads:",c*a)
+print("the amount of day old breads:",discount*b)
+print("Total amount:",(c*a)+(discount*b))
