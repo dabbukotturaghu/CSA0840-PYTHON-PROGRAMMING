0e53f456e71ddd4f5944361ced69c05efe51fdc8
dabbukotturaghu <112841745+dabbukotturaghu@users.noreply.github.com>
2022-09-21 13:21:36 +0530

    Add files via upload

---
+++ MERGE 2 SORTED LIST.py
@@ -0,0 +1,6 @@
+test_list1=[1,2,4]
+test_list2=[1,3,4]
+print("the list 1 is:"+str(test_list1))
+print("the list 2 is:"+str(test_list2))
+res=sorted(test_list1+test_list2)
+print("the combined sorted list is :"+str(res))
